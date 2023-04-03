4651efa4553b0f3ef90f4a1ee3f5ca166dc35c94
Paulo de Souza <paulodesouza.tech@gmail.com>
2023-04-03 17:31:34 -0400

    Finished conditional validation of date field.

--- excel-to-yaml.py
+++ excel-to-yaml.py
@@ -19,7 +19,7 @@ file_list = get_file_names()
 
 for file in file_list:
     
-    if ".xlsx" in file:
+    if "book.xlsx" == file:
         
         wb = load_workbook(file)
         ws = wb.active
@@ -46,6 +46,8 @@ for file in file_list:
                            ]
         
         date = ws["I2"].value
+        date_obj = datetime(int(date[-4:]), int(date[3:5]), int(date[:2])) 
+        
         needs_to_be_string = ["F2", "B4", "B15", "B28"]
         needs_to_be_int = ["D4", "F4", "H4", "J4",
                            "B5", "B6",  
@@ -71,6 +73,7 @@ for file in file_list:
         
         if "-" not in date:
             on_error("Invalid Date Format")
+            
         else:
             date = date.split("-")
             if len(date) == 3 and len(date[0]) == 2 and len(date[1]) == 2 and len(date[2]) == 4:
@@ -78,6 +81,12 @@ for file in file_list:
             else: 
                 on_error("Invalid Date Format")
                 keep_going = False
+                
+        if date_obj.strftime("%A") == "Friday":
+            pass
+        else:
+            on_error("Date of report needs to be FRIDAY!")
+            keep_going = False
         
         
         for cell in cannot_be_empty:
@@ -105,9 +114,10 @@ for file in file_list:
                 
         
         
-        if len(empty) > 0 and keep_going:
+        if len(empty) > 0:
             
             on_error(f"Necessary fields are still empty! Please fill these out before running the program: {empty}")
+            keep_going = False
             
             
         
@@ -115,12 +125,14 @@ for file in file_list:
         elif len(isnt_string) > 0:
             
             on_error(f"These fields should have Words in them, not numbers: {isnt_string}")
+            keep_going = False
             
         elif len(isnt_int) > 0:
             
             on_error(f"These fields should have Numbers in them, and not alphabetical characters: {isnt_int}")
+            keep_going = False
             
-        else:
+        if keep_going:
             
             data = {}
             
@@ -141,37 +153,40 @@ for file in file_list:
                 "Actual FTEs": ws["B6"].value,
                 "FTEs Starting Date (Actual)": ws["D6"].value,
                 
-                "Inbound Call Goal & Actuals" : [
-                     ws["B9"].value, ws["D9"].value, ws["E9"].value, 
-                     ws["F9"].value, ws["G9"].value, ws["H9"].value,  
-                     ws["I9"].value,  ws["J9"].value 
+                "GoalInbound": ws["B9"].value,
+                "Inbound Call Actuals" : [
+                      ws["D9"].value, ws["E9"].value, 
+                     ws["F9"].value, ws["G9"].value, ws["H9"].value
+                     
                      ],
                 
                 "TotalInbound" : ws["D9"].value + ws["E9"].value +
                 ws["F9"].value + ws["G9"].value + ws["H9"].value,
                 
-                "Outbound Call Goal & Actuals" : [
-                    ws["B10"].value, ws["D10"].value, ws["E10"].value, 
-                    ws["F10"].value, ws["G10"].value, ws["H10"].value,  
-                    ws["I10"].value,  ws["J10"].value 
+                
+                "GoalOutbound": ws["B10"].value,
+                "Outbound Call Actuals" : [
+                    ws["D10"].value, ws["E10"].value, 
+                    ws["F10"].value, ws["G10"].value, ws["H10"].value
                     ],
                 
                 "TotalOutbound" : ws["D10"].value + ws["E10"].value +
                 ws["F10"].value + ws["G10"].value + ws["H10"].value,
                 
-                "Visit Schedule Goal & Actuals" : [
-                    ws["B11"].value, ws["D11"].value, ws["E11"].value, 
-                    ws["F11"].value, ws["G11"].value, ws["H11"].value,  
-                    ws["I11"].value,  ws["J11"].value 
+                
+                "GoalVisit": ws["B11"].value,
+                "Visit Schedule Actuals" : [
+                    ws["D11"].value, ws["E11"].value, 
+                    ws["F11"].value, ws["G11"].value, ws["H11"].value
                     ],
                 
                 "TotalVisit" : ws["D11"].value + ws["E11"].value +
                 ws["F11"].value + ws["G11"].value + ws["H11"].value,
                 
-                "Enrolled Goal & Actuals" : [
-                    ws["B12"].value, ws["D12"].value, ws["E12"].value, 
-                    ws["F12"].value, ws["G12"].value, ws["H12"].value, 
-                    ws["I12"].value,  ws["J12"].value 
+                "GoalEnrolled": ws["B12"].value,
+                "Enrolled Actuals" : [
+                    ws["D12"].value, ws["E12"].value, 
+                    ws["F12"].value, ws["G12"].value, ws["H12"].value 
                     ],
                 
                 "TotalEnrolled" : ws["D12"].value + ws["E12"].value +
@@ -193,38 +208,39 @@ for file in file_list:
                 "Actual FTEs": ws["B17"].value,
                 "FTEs Starting Date (Actual)": ws["D17"].value,
                 
-                
-                "Inbound Call Goal & Actuals" : [
-                     ws["B20"].value, ws["D20"].value, ws["E20"].value,
-                     ws["F20"].value, ws["G20"].value, ws["H20"].value,
-                     ws["I20"].value, ws["J20"].value
+                "GoalInbound": ws["B20"].value,
+                "Inbound Call Actuals" : [
+                     ws["D20"].value, ws["E20"].value,
+                     ws["F20"].value, ws["G20"].value, ws["H20"].value
                      ],
                 
                 "TotalInbound" : ws["D20"].value + ws["E20"].value +
                 ws["F20"].value + ws["G20"].value + ws["H20"].value,
                 
-                "Outbound Call Goal & Actuals" : [
-                     ws["B21"].value, ws["D21"].value, ws["E21"].value, 
-                     ws["F21"].value, ws["G21"].value, ws["H21"].value,  
-                     ws["I21"].value, ws["J21"].value 
+                "GoalOutbound": ws["B21"].value, 
+                "Outbound Call Actuals" : [
+                     ws["D21"].value, ws["E21"].value, 
+                     ws["F21"].value, ws["G21"].value, ws["H21"].value 
                      ],
                 
                 "TotalOutbound" : ws["D21"].value + ws["E21"].value +
                 ws["F21"].value + ws["G21"].value + ws["H21"].value,
                 
-                "Visit Schedule Goal & Actuals" : [
-                     ws["B22"].value, ws["D22"].value, ws["E22"].value, 
-                     ws["F22"].value, ws["G22"].value, ws["H22"].value,
-                     ws["I22"].value, ws["J22"].value
+                
+                "GoalVisit": ws["B22"].value, 
+                "Visit Schedule Actuals" : [
+                     ws["D22"].value, ws["E22"].value, 
+                     ws["F22"].value, ws["G22"].value, ws["H22"].value
                      ],
                 
                 "TotalSchedule" : ws["D22"].value + ws["E22"].value + 
                 ws["F22"].value + ws["G22"].value + ws["H22"].value,
                 
-                "Enrolled Goal & Actuals" : [
-                     ws["B23"].value, ws["D23"].value, ws["E23"].value, 
-                     ws["F23"].value, ws["G23"].value, ws["H23"].value,
-                     ws["I23"].value, ws["J23"].value 
+                
+                "GoalEnrolled": ws["B23"].value, 
+                "Enrolled Actuals" : [
+                     ws["D23"].value, ws["E23"].value, 
+                     ws["F23"].value, ws["G23"].value, ws["H23"].value
                      ],
                 
                 "TotalEnrolled" : ws["D23"].value + ws["E23"].value +
@@ -253,6 +269,8 @@ for file in file_list:
             
             
             # Output to YAML
+            
+            # APPROXIMATE THE DATE TO FRIDAY HERE.
             
             with open(f"{data['EA'].upper()}_{data['Week']}.yaml", "w") as file:
                 yaml.dump(data, file, default_flow_style=False)
